8d40dd2bf1c3fb1884728bceaeede57dbf0b2fa0
kfrapin <kevin.frapin@gmail.com>
2020-07-30 15:46:02 +0200

    fix(itp-39): change aws hosted connection param

--- examples/aws-hosted-azure-example/main.tf
+++ examples/aws-hosted-azure-example/main.tf
@@ -10,6 +10,7 @@ terraform {
     aws     = "~> 2.58"
     random  = "~> 2.2"
     azurerm = "~> 2.6"
+
   }
 }
 
@@ -50,7 +51,7 @@ resource "tls_private_key" "common" {
 
 
 data "intercloud_destinations" "dest_aws" {
-  family = "aws"
+  family = "awshostedconnection"
 }
 
 data "intercloud_destinations" "dest_azure" {
@@ -64,7 +65,7 @@ data "intercloud_destination" "aws_destination" {
 }
 
 data "intercloud_destination" "azure_destination" {
-  location = "equinix ld5 london"
+  location = "Amsterdam"
   #location = "equinix am2 amsterdam"
   family = "azure"
 }
